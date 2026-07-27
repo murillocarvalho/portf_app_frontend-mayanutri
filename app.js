7f5c3de11e31e4d1c7af1a1b39f93768dc87068f
Murillo Carvalho <murillohpcarvalho@gmail.com>
2026-07-27 20:24:24 -0300

    test - change "health" method

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/perguntas', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'perguntas.html'));
 });
 
-app.post('/health', (req, res) => {
+app.get('/health', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'health.html'));
 });
 
